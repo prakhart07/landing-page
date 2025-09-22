d9108cf6efb93688489a65301bfc8a44cacd7758
Vinayak Rathore <vinayakrathore441@gmail.com>
2025-09-22 15:46:18 +0530

    newnavbar

---
+++ src/Components/navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "../Assests/CSS/Main.css"
+function Navbar(){
+
+    return(
+         <nav className="header-nav">
+        <div className="container flex space-between"></div>
+         {/* BRANDING */}
+          <div className="flex align-center brand">
+            <a href="#">
+              <img className="logo" alt="Carved Rock Fitness" src="img/carved-rock-logo.png" />
+                </a>
+          </div>
+          {/* SEARCH */}
+          <div className="flex flex-one align-center">
+            <div>
+              <form>
+                <input className="search-box" aria-label="Search" placeholder="What can we help you find?" type="search" />
+                <button className="search-button" type="submit">SEARCH</button>
+              </form>
+              <ul className="flex space-between nav-list">
+                <li><a href="#">Clothing</a></li>
+                <li><a href="#">Footwear</a></li>
+                <li><a href="#">Equipment</a></li>
+                <li><a href="#">Bags &amp; Travel</a></li>
+                <li><a href="#">Trail Reviews</a></li>
+              </ul>
+            </div>
+            {/* PROFILE */}
+            <div className="flex align-center profile">
+              <div className="flex flex-column account">
+                John Doe <a href="#">Log Out</a>
+              </div>
+              <div className="profile-pic"><img alt="John Doe" src="img/profile-pic.jpg" /></div>
+            </div>
+          </div>
+      
+      </nav>
+    )
+}
+export default Navbar;
+    
