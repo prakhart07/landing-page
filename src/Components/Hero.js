64c5ba258db664f6f57d94c4b706c1cdfa65da1d
shobhit786999 <shobhitraghuwanshi07@gmail.com>
2025-09-22 13:52:54 +0530

    Hero.js

---
+++ src/Components/Hero.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../Assests/CSS/Main.css"
+
+function Hero(){
+    return(
+        <section className="hero"  >
+            <div className="container flex flex-column align-center center">
+                <h1>Get A Grip</h1>
+                <h2>20% Off</h2>
+                <p>Throughout The Season</p>
+            </div>
+      </section >
+    );
+}
+export default Hero;
