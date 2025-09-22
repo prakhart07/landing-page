9621bd4eac94bc77776eabbaa2244a899dab2a0c
Vinayak Rathore <vinayakrathore441@gmail.com>
2025-09-22 21:37:57 +0530

    app routing update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import logo from './logo.svg';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Navbar from './Components/navbar';
+import Categories from './Components/Categories';
+import FullBanner from './Components/FullBanner';
+
+import Hero from "./Components/Hero";
+ 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/navbar" element={<Navbar />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/fullbanner" element={<FullBanner />} />
+        <Route path="/hero" element={<Hero />} />
         </Routes>
     </Router>
 
